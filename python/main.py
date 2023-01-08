55e89fb35206c574cfdb4ec87433526b077df60d
Aleksander Evensen <ahse03@gmail.com>
2023-01-08 02:25:56 +0100

    feat: Synrtax error checking

--- python/main.py
+++ python/main.py
@@ -1,30 +1,5 @@
 def test():
-    print("This is a test")
+	print("Hello World")
+	def hello {aq}:
 
-    def test2(a):
-        print("This is test 2")
-
-    test2()
-
-
-def test5(name, age, test=True, other=False):
-    print(f"{name} is {age} years old")
-
-
-class MyClass:
-    def __init__(self):
-        print(self)
-
-
-x = 5
-
-y = x < z
-
-global t
-
-if x > 10:
-    q = 10
-    print("Hello World")
-y = x
-
-print("Hello World")
+print("Hello World Outside")
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,65 @@
 #![allow(unused_variables)]
 
 mod symboltree;
-use tree_sitter::Parser;
+mod utils;
 
 use crate::symboltree::SymbolTree;
+use anyhow::{Context, Result};
+use tree_sitter::{Node, Parser, Tree, TreeCursor};
+use utils::GetStringFromNode;
 
-fn main() {
+fn main() -> Result<()> {
     let code = std::fs::read_to_string("./python/main.py").unwrap();
 
     let mut parser = Parser::new();
     parser
         .set_language(tree_sitter_python::language())
-        .expect("Failed to load the python tree-sitter grammars");
+        .context("Failed to load the python tree-sitter grammars")?;
 
     let ast = parser
         .parse(&code, None)
-        .expect("Failed to parse the python code");
+        .context("Failed to parse the python code")?;
+
+    check_syntax_errors(&ast, &code).unwrap();
 
     let symtree = SymbolTree::from_ast(&ast, &code, "main.py".to_string()).unwrap();
 
     println!("{symtree:#?}");
+
+    Ok(())
+}
+
+fn check_syntax_errors(ast: &Tree, code: &String) -> Result<()> {
+    fn find_error_node<'a>(cursor: &mut TreeCursor<'a>) -> Option<Node<'a>> {
+        cursor.goto_first_child();
+
+        loop {
+            if cursor.node().is_error() {
+                return Some(cursor.node());
+            }
+
+            if cursor.node().child_count() > 0 {
+                if let Some(error_node) = find_error_node(cursor) {
+                    return Some(error_node);
+                }
+            }
+
+            if !cursor.goto_next_sibling() {
+                break;
+            }
+        }
+        cursor.goto_parent();
+        None
+    }
+
+    let mut cursor = ast.root_node().walk();
+
+    if let Some(error_node) = find_error_node(&mut cursor) {
+        return Err(anyhow::anyhow!(
+            "Found an error '{}' on Line {}",
+            error_node.get_string(code),
+            error_node.start_position().row + 1,
+        ));
+    }
+    Ok(())
 }
